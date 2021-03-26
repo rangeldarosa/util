1fe30ff9b6bebfc73af2cf4170ba868284bf2760
rangeldarosa <rangel.darosa@outlook.com>
2021-03-25 21:00:49 -0300

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -4,5 +4,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -;
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable";
 apt update;
 apt-cache policy docker-ce;
-apt install docker-ce;
+apt install -y docker-ce;
 systemctl status docker;
